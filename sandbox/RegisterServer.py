d018ffc00d3f39f08373e6128319327a3a0c291c
Athanaseus <ramaila.jat@gmail.com>
2016-02-23 15:15:52 +0200

    Basic script for server registration

--- sandbox/RegisterServer.py
+++
@@ -1,9 +0,0 @@
-import PyTango
-
-dev_info = PyTango.DbDevInfo()
-dev_info.server = "AntennaPositioner/test"
-dev_info._class = "AntennaPositioner"
-dev_info.name = "test/antenna_positioner/1"
-db = PyTango.Database()
-db.add_device(dev_info)
-print "Registration Successful"
