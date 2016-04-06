fcbaab025bb31565358483be24134b2782c99795
kmadisa <kmadisa@ska.ac.za>
2016-04-06 16:30:04 +0200

    Creating the attributes according to the sensor_type value

--- mkat_tango/simulators/MkatAntennaPositioner.py
+++ mkat_tango/simulators/MkatAntennaPositioner.py
@@ -15,53 +15,45 @@ MeerKAT AP simulator.
 from mkat_ap import MkatApModel
 
 
-import time
-import numpy
-import threading
-import logging
-
-from functools import partial
 
 
 from PyTango.server import device_property
 from PyTango.server import Device, DeviceMeta, attribute, command, server_run
 
-from PyTango import AttrQuality, AttrWriteType, DispLevel, DevState, DebugIt, Attr, DevFloat, DevString, DevBoolean, DevDouble
+from PyTango import AttrQuality, AttrWriteType, DispLevel, DevState, DebugIt
+from PyTango import  Attr, DevFloat, DevString, DevBoolean, DevDouble
 from PyTango import UserDefaultAttrProp
 
 class MkatAntennaPositioner(Device):
     __metaclass__ = DeviceMeta
-    
-    #voltage = attribute()   
     
     def init_device(self):
         Device.init_device(self)
         self.set_state(DevState.STANDBY)
         
         ap_model = MkatApModel()
-        #ap_model.start()
-        print "starting"
         sensors = ap_model.get_sensors()
         
         attr_props = UserDefaultAttrProp()
         
         for sensor in sensors:
             print sensor.name
-#            if sensor.stype == "boolean":
-#                attr = Attr(sensor.name, DevBoolean)
-#            elif sensor.stype == "float":
-#                attr = Attr(sensor.name, DevFloat)
-#            elif sensor.stype == "discrete":
-#                attr = Attr(sensor.name, DevString)
             
-            attr = Attr(sensor.name, DevDouble)
+            if sensor.stype == "boolean":
+                attr = Attr(sensor.name, DevBoolean)
+            elif sensor.stype == "float":
+                attr = Attr(sensor.name, DevFloat)
+            elif sensor.stype == "discrete":
+                attr = Attr(sensor.name, DevString)
+            
             attr_props.set_label(sensor.name)
             attr_props.set_description(sensor.description)
             attr_props.set_unit(sensor.units)
             attr.set_default_properties(attr_props)
             
             self.add_attribute(attr)
-
+    
+    
 
 if __name__ == "__main__":
          server_run([MkatAntennaPositioner])
