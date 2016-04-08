4cd5648f13f8fc947b3143f5a83e684a60b22b15
kmadisa <kmadisa@ska.ac.za>
2016-04-08 11:24:16 +0200

    Adding test cases for the MkatAP

---
+++ mkat_tango/simulators/test_MkatAntennaPositioner.py
@@ -0,0 +1,53 @@
+KA South Africa (http://ska.ac.za/)                                        #
+# Author: cam@ska.ac.za                                                       #
+# Copyright @ 2013 SKA SA. All rights reserved.                               #
+#                                                                             #
+# THIS SOFTWARE MAY NOT BE COPIED OR DISTRIBUTED IN ANY FORM WITHOUT THE      #
+# WRITTEN PERMISSION OF SKA SA.                                               #
+###############################################################################
+"""
+MeerKAT AP simulator.
+    @author MeerKAT CAM team <cam@ska.ac.za>
+"""
+
+from devicetest import DeviceTestCase
+import logging
+
+class MkatAntennaPositionerTest(DeviceTestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+    def test_model_instantiation(self):
+        pass
+
+    def test_model_thread_started(self):
+        pass
+
+    def test_stop(self):
+        pass
+  
+    def test_slew(self):
+        pass
+
+    def test_maintenance(self):
+        pass
+
+    def test_rate(self):
+        pass
+
+    def test_stow(self):
+        pass
+
+    def test_attribute_values(self):
+        pass
+
+    def test_write_attributes(self):
+        pass
+
+    
+    
