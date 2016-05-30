fe6587ac0678056a7169db9c878949650aa20061
Neilen Marais <nmarais@ska.ac.za>
2016-05-30 17:48:36 +0200

    Remove special case for DevState in request handler
    Instead implement a new kattype (subclass of Discrete) that knows about the
    tango DevState enumeration.

--- mkat_tango/translators/katcp_tango_proxy.py
+++ mkat_tango/translators/katcp_tango_proxy.py
@@ -70,6 +70,18 @@ TANGO_NUMERIC_TYPES = TANGO_FLOAT_TYPES | TANGO_INT_TYPES
 TANGO_CMDARGTYPE_NUM2NAME = {num: name
                              for name, num in PyTango.CmdArgType.names.items()}
 
+class TangoStateDiscrete(kattypes.Discrete):
+    """A kattype that is compatible with the PyTango.DevState enumeration"""
+
+    def check(self, value, major):
+        return super(TangoStateDiscrete, self).check(str(value), major)
+
+    def encode(self, value, major):
+        return super(TangoStateDiscrete, self).encode(str(value), major)
+
+    def decode(self, value, major):
+        return getattr(PyTango.DevState, value)
+
 TANGO2KATCP_TYPE_INFO = {
     DevFloat: KatcpTypeInfo(KatcpType=kattypes.Float, sensor_type=Sensor.FLOAT,
                             params=dtype_params(np.float32)),
@@ -95,8 +107,9 @@ TANGO2KATCP_TYPE_INFO = {
                              params=()),
     DevEnum: KatcpTypeInfo(KatcpType=kattypes.Discrete, sensor_type=Sensor.DISCRETE,
                            params=()),
-    CmdArgType.DevState: KatcpTypeInfo(KatcpType=kattypes.Discrete, sensor_type=Sensor.DISCRETE,
-                           params=(DevState.names.keys()))
+    CmdArgType.DevState: KatcpTypeInfo(
+        KatcpType=TangoStateDiscrete, sensor_type=Sensor.DISCRETE,
+        params=(DevState.names.keys()))
 }
 
 
@@ -214,8 +227,6 @@ def tango_cmd_descr2katcp_request(tango_command_descr, tango_device_proxy):
         # A reference for debugging ease so that it is in the closure
         tango_device_proxy
         tango_retval = yield tango_request(cmd_name)
-        if isinstance(tango_retval, PyTango._PyTango.DevState):
-            tango_retval = str(tango_retval)
         raise Return(
             ('ok', tango_retval) if tango_retval is not None else ('ok', ))
 
@@ -274,8 +285,10 @@ def tango_type2kattype_object(tango_type):
     if tango_type in TANGO_NUMERIC_TYPES:
         kattype_kwargs['min'], kattype_kwargs['max'] = katcp_type_info.params
     elif tango_type == CmdArgType.DevState:
-         kattype_kwargs = [name for name in katcp_type_info.params]
-         return katcp_type_info.KatcpType(kattype_kwargs)
+        # TODO (NM, KM) 2016-05-30 can we get rid of this if statement by using
+        # TANGO2KATCP_TYPE_INFO better?
+        kattype_kwargs = [name for name in katcp_type_info.params]
+        return katcp_type_info.KatcpType(kattype_kwargs)
     # TODO NM We should be able to handle the DevVar* variants by checking if
     # in_type.name starts with DevVar, and then lookup the 'scalar' type. The we
     # can just use multiple=True on the KATCP type
