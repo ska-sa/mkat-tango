03df3068afd7b6281d6e316bd770b472f5d45365
athanaseus <aramaila@ska.ac.za>
2016-06-10 08:58:36 +0200

    Added tests to quantify that the model variables change through the simulator test interface device

--- mkat_tango/simlib/tests/test_sim_test_interface.py
+++ mkat_tango/simlib/tests/test_sim_test_interface.py
@@ -3,6 +3,8 @@ import mock
 
 from functools import partial
 
+from addict import Dict
+
 from devicetest import DeviceTestCase
 
 from mkat_tango.simlib import sim_test_interface, model, quantities
@@ -19,6 +21,12 @@ class FixtureModel(model.Model):
         self.sim_quantities['relative-humidity'] = GaussianSlewLimited(
             mean=65, std_dev=10, max_slew_rate=10,
             min_bound=0, max_bound=150)
+        self.sim_quantities['wind-speed'] = GaussianSlewLimited(
+            mean=1, std_dev=20, max_slew_rate=3,
+            min_bound=0, max_bound=100)
+        self.sim_quantities['wind-direction'] = GaussianSlewLimited(
+            mean=0, std_dev=150, max_slew_rate=60,
+            min_bound=0, max_bound=359.9999)
         self.sim_quantities['input-comms-ok'] = ConstantQuantity(start_value=True)
         super(FixtureModel, self).setup_sim_quantities()
 
@@ -36,11 +44,27 @@ class test_SimControl(DeviceTestCase):
     def setUp(self):
         super(test_SimControl, self).setUp()
         self.test_model.reset_model()
-        self.control_attributes = []
+        self.control_attributes = self._control_attributes()
         self.device_instance = sim_test_interface.SimControl.instances[
                 self.device.name()]
         def cleanup_refs(): del self.device_instance
         self.addCleanup(cleanup_refs)
+
+    def _control_attributes(self):
+        """Function collects all the available models and gets all the
+        adjustable_attributes which will be control attributes on the
+        simulator test interface device.
+        Returns
+        =======
+        control_attributes : list
+            A list of all the adjustable attributes
+        """
+        control_attributes = []
+        models = set([quant.__class__
+                for quant in self.test_model.sim_quantities.values()])
+        for cls in models:
+            control_attributes += [attr for attr in cls.adjustable_attributes]
+        return control_attributes
 
     def test_attribute_list(self):
         ADDITIONAL_IMPLEMENTED_ATTR = set([
@@ -49,28 +73,44 @@ class test_SimControl(DeviceTestCase):
             'sensor_name',    # Attribute indentifier for sensor to be controlled
             'pause_active',    # Flag for pausing the model updates
             ])
-        models = set([quant.__class__
-            for quant in self.test_model.sim_quantities.values()])
         attributes = set(self.device.get_attribute_list())
-        for cls in models:
-            self.control_attributes += [attr for attr in cls.adjustable_attributes]
         self.assertEqual(attributes - ADDITIONAL_IMPLEMENTED_ATTR,
                         set(self.control_attributes))
 
     def test_model_defaults(self):
         device_model = self.device_instance.model
-        # test that the model instance of the sim control is the one same as Fixture
+        # test that the model instance of the sim control is the same one as Fixture
         self.assertEqual(device_model, self.test_model)
         # test that default values from the instantiated model match that of sim control
-        for quantity in device_model.sim_quantities.keys():
+        for quantity in set(device_model.sim_quantities.keys()):
             model_quantity_type = device_model.sim_quantities[quantity]
             fixture_quantity_type = self.test_model.sim_quantities[quantity]
             self.assertEqual(model_quantity_type, fixture_quantity_type)
-            #from IPython import embed; embed()
             self.device.sensor_name = quantity  # sets the sensor name for which
             # to evaluate the quantities to be controlled
             for attr in self.device.get_attribute_list():
-                if attr in self.control_attributes:
-                    attribute_value = getattr(self.device, attr, None)
-                    model_attr_value = getattr(fixture_quantity_type, None)
-                    self.assertEqual(attribute_value, model_attr_value)
+                if attr in model_quantity_type.adjustable_attributes:
+                    attribute_value = getattr(self.device, attr)
+                    model_attr_value = getattr(fixture_quantity_type, attr)
+                    if model_attr_value:
+                        self.assertEqual(attribute_value, model_attr_value)
+
+    def test_model_attribute_change(self):
+        desired_sensor_name = 'relative-humidity'
+        control_attr_dict = Dict()
+        control_attr_dict.desired_mean = 60
+        control_attr_dict.desired_min_bound = 5
+        control_attr_dict.desired_max_bound = 100
+        control_attr_dict.desired_std_dev = 2
+        control_attr_dict.desired_max_slew_rate = 2
+        control_attr_dict.desired_last_val = 62
+        control_attr_dict.desired_last_update_time = time.time()
+        self.device.sensor_name = desired_sensor_name
+        # setting the desired attribute values for the device's attributes
+        # that can be controlled
+        for attr in self.device.get_attribute_list():
+            if attr in self.control_attributes:
+                val = getattr(control_attr_dict, 'desired_' + attr)
+                setattr(self.device, attr, val)
+        # Here the test_model_defaults is called to quanify the changes on the model
+        self.test_model_defaults()
