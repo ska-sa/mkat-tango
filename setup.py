ff3edba8dfffddd922bdb9279da713cd8f779040
Martin Slabber <martin.slabber@gmail.com>
2016-08-10 12:51:44 +0200

    Change Setup.py to include the requirements for testing.

--- setup.py
+++ setup.py
@@ -1,29 +1,43 @@
 #!/usr/bin/env python
+
 from setuptools import setup, find_packages
 
-setup (
-    name = "mkat_tango",
-    version = "0.0.0dev",
-    description = " Work relating to the use of tango in MeerKAT and for SKA",
-    author = "SKA SA KAT-7 / MeerKAT CAM team",
-    author_email = "cam@ska.ac.za",
-    packages = find_packages(),
-    url='https://github.com/ska-sa/mkat-tango',
-    classifiers=[
-        "Intended Audience :: Developers",
-        "Programming Language :: Python :: 2",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Scientific/Engineering :: Astronomy",
-    ],
-    platforms = [ "OS Independent" ],
-    install_requires = ["PyTango>=8.1.5", "numpy", "enum", "katcp"],
-    zip_safe = False,
-    entry_points={
-    'console_scripts': [
-        'mkat-tango-weather-DS = mkat_tango.simulators.weather:weather_main',
-        'mkat-tango-AP-DS = mkat_tango.simulators.mkat_ap_tango:main',
-        'mkat-tango-tangodevice2katcp = mkat_tango.translators.katcp_tango_proxy:tango2katcp_main',
-        'mkat-tango-katcpdevice2tango-DS = mkat_tango.translators.tango_katcp_proxy:main',
-        'mkat-tango-tango_launcher = mkat_tango.translators.tango_launcher:main',
-    ]},
-)
+setup(name="mkat_tango",
+      description=" Work relating to the use of tango in MeerKAT and for SKA",
+      author="SKA SA KAT-7 / MeerKAT CAM team",
+      author_email="cam@ska.ac.za",
+      packages=find_packages(),
+      url='https://github.com/ska-sa/mkat-tango',
+      classifiers=[
+          "Intended Audience :: Developers",
+          "Programming Language :: Python :: 2",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "Topic :: Scientific/Engineering :: Astronomy",
+      ],
+      platforms=["OS Independent"],
+      setup_requires=["katversion"],
+      use_katversion=True,
+      install_requires=[
+          "PyTango>=8.1.5",
+          "numpy",
+          "enum",
+          "katcore",
+          "katproxy",
+          "katcp"],
+      test_require=[
+          'enum',
+          'numpy',
+          'nose_xunitmp',
+          'python-devicetest'],
+      zip_safe=False,
+      dependency_links=[
+          'git+https://github.com/vxgmichel/pytango-devicetest.git#egg=python_devicetest'],
+      entry_points={
+          'console_scripts': [
+              'mkat-tango-weather-DS = mkat_tango.simulators.weather:weather_main',
+              'mkat-tango-AP-DS = mkat_tango.simulators.mkat_ap_tango:main',
+              'mkat-tango-tangodevice2katcp = mkat_tango.translators.katcp_tango_proxy:tango2katcp_main',
+              'mkat-tango-katcpdevice2tango-DS = mkat_tango.translators.tango_katcp_proxy:main',
+              'mkat-tango-tango_launcher = mkat_tango.translators.tango_launcher:main',
+          ]},
+      )
