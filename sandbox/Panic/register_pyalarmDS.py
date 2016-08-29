07c08462d155db0e35055341fc6f0375c25cf247
kmadisa <kmadisa@ska.ac.za>
2016-08-29 12:45:22 +0200

    Separating PyAlarmDS registration from alarm object creation

---
+++ sandbox/Panic/register_pyalarmDS.py
@@ -0,0 +1,22 @@
+import PyTango
+import subprocess
+
+db = PyTango.Database()
+
+def add_new_device(server,klass,device):
+    dev_info = PyTango.DbDevInfo()
+    dev_info.name = device
+    dev_info.klass = klass
+    dev_info.server = server
+    db.add_device(dev_info)
+
+# Create a PyAlarm device
+add_new_device('PyAlarm/kataware','PyAlarm','mkat/panic/kataware')
+
+# PyAlarm device server name alarms will be exported to as attributes
+pyalarm_name = 'mkat/panic/kataware'
+
+print("Succefully registered PyAlarms in Tango-DB")
+print("==========================================")
+print("Attempting to run PyAlarm Device Server...")
+subprocess.check_call(["PyAlarm", "kataware"])
