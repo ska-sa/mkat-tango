448b2d4bc3af7fa5bc431c6bd704b6a22ee2b773
athanaseus <aramaila@ska.ac.za>
2016-09-30 17:10:32 +0200

    Addressing comments w.r.t. PR#100 (a)

--- mkat_tango/simlib/sim_xmi_parser.py
+++ mkat_tango/simlib/sim_xmi_parser.py
@@ -24,20 +24,41 @@ POGO2TANGO_TYPE = {
 
 class Xmi_Parser(object):
 
-    def __init__(self, xmi_file=None):
+    def __init__(self, xmi_file):
         self.xmi_file = xmi_file
         self.device_class_name = ''
         self.device_attributes = []
+        """The Data structure format is a list containing attribute info in a dict.
+        e.g.[{'name': 'wind_speed', 'dataShape': 'Scalar',
+            'dataType': tango._tango.CmdArgType.DevDouble,
+            'description': 'Wind speed in central telescope area.',
+            'displayLevel': 'EXPERT', 'label': 'Wind speed', 'maxAlarm': '25',
+            'maxValue': '30', 'maxWarning': '15', 'minAlarm': '', 'minValue': '0',
+            'minWarning': '','name': 'wind_speed', 'polledPeriod': '3000',
+            'readWriteProperty': 'READ', 'unit': 'm/s'}, ...]
+
+        """
         self.device_commands = []
+        """The Data structure format is a list containing command info in a dict.
+        e.g.[{'name': 'On', 'arginDescription': '',
+            'arginType': tango._tango.CmdArgType.DevVoid,
+            'argoutDescription': 'ok | Device ON',
+            'argoutType': tango._tango.CmdArgType.DevString,
+            'description': 'Turn On Device'}, ...]
+
+        """
         self.device_properties = []
-        if self.xmi_file:
-            self.sim_description_data()
-        else:
-            raise IOError("No XMI file specified.")
+        """The Data structure format is a list containing device property info in a
+            dict. e.g.[{'name': 'katcp_address', 'defaultPropValue': '127.0.0.1',
+                  'description': '', 'type': tango._tango.CmdArgType.DevString},
+
+        """
+        self.sim_description_data()
 
     def sim_description_data(self):
-        """This methods adds all the simulator description data from the xmi
-        tree to appropriate data structures.
+        """This method staores all the simulator description data from the xmi
+        tree into appropriate data structures (dict) and then append it to the
+        Xmi_Parser attributes.
 
         """
         try:
@@ -45,38 +66,46 @@ class Xmi_Parser(object):
             root = tree.getroot()
             device_class = root.find('classes')
             self.device_class_name = device_class.attrib['name']
-            self.description_data = device_class.find('description')
-            for description_data in device_class:
-                if description_data.tag in ['commands']:
-                    command = self.command_description_data(description_data)
-                    self.device_commands.append(command)
-                elif description_data.tag in ['dynamicAttributes', 'attributes']:
-                    attribute = self.attributes_description_data(description_data)
-                    self.device_attributes.append(attribute)
-                elif description_data.tag in ['deviceProperties']:
-                    device_property = self.device_property_description_data(
-                            description_data)
-                    self.device_properties.append(device_property)
-        except IOError as nierr:
-            MODULE_LOGGER.info(str(nierr))
+            for class_description_data in device_class:
+                if class_description_data.tag in ['commands']:
+                    command_info = self.command_description_data(class_description_data)
+                    self.device_commands.append(command_info)
+                elif class_description_data.tag in ['dynamicAttributes', 'attributes']:
+                    attribute_info = self.attributes_description_data(
+                                                class_description_data)
+                    self.device_attributes.append(attribute_info)
+                elif class_description_data.tag in ['deviceProperties']:
+                    device_property_info = self.device_property_description_data(
+                                                           class_description_data)
+                    self.device_properties.append(device_property_info)
+        except IOError:
+            MODULE_LOGGER.info("No such file or directory: " + self.xmi_file,
+                               exc_info=True)
 
     def command_description_data(self, description_data):
         """Extract command description data from the xmi tree element.
 
         Parameters
-        ---------
+        ----------
         description_data: xml.etree.ElementTree.Element
-            XMI tree element with command data
+            XMI tree element with command data, where
+            expected element tag(s) are (i.e. description_data.tag)
+            ['argin', 'argout'] and
+            description_data.attrib contains
+            {'description': 'Turn On Device',
+            'displayLevel': 'OPERATOR',
+            'execMethod': 'on',
+            'isDynamic': 'false',
+            'name': 'On',
+            'polledPeriod': '0'}
 
         Returns
         -------
         command_data: dict
-            Dictionary of command data as prescribed in the SIMDD
+            Dictionary of all the command data required to create a tango command
 
         """
-        command_data = {}
-        command_data['name'] = description_data.attrib['name']
-        command_data['description'] = description_data.attrib['description']
+        command_data = description_data.attrib
         input_parameter = description_data.find('argin')
         command_data['arginDescription'] = input_parameter.attrib['description']
         command_data['arginType'] = self._get_arg_type(input_parameter)
@@ -89,52 +118,65 @@ class Xmi_Parser(object):
         """Extract attribute description data from the xmi tree element.
 
         Parameters
-        ---------
+        ----------
         description_data: xml.etree.ElementTree.Element
             XMI tree element with attribute data
+
+            Expected element tag(s) are (i.e. description_data.tag)
+            ['properties']
+
+            description_data.find('properties').attrib contains
+            {'deltaTime': '', 'deltaValue': '', 'description': '',
+             'displayUnit': '', 'format': '', 'label': '',
+             'maxAlarm': '', 'maxValue': '', 'maxWarning': '',
+             'minAlarm': '', 'minValue': '', 'minWarning': '',
+             'standardUnit': '', 'unit': ''} and
+
+            description_data.attrib contains
+            {'allocReadMember': 'false',
+            'attType': 'Scalar',
+            'displayLevel': 'OPERATOR',
+            'isDynamic': 'false',
+            'maxX': '',
+            'maxY': '',
+            'name': 'Constant',
+            'polledPeriod': '0',
+            'rwType': 'WRITE'}
 
         Returns
         -------
         attribute_data: dict
-            Dictionary of attribute data as prescribed in the SIMDD
+            Dictionary of all attribute data required to create a tango attribute
 
         """
-        attribute_data = {}
+        attribute_data = description_data.attrib
         attribute_properties = description_data.find('properties')
-        attribute_data['name'] = description_data.attrib['name']
-        attribute_data['dataShape'] = description_data.attrib['attType']
-        attribute_data['readWriteProperty'] = description_data.attrib['rwType']
-        attribute_data['displayLevel'] = description_data.attrib['displayLevel']
-        attribute_data['polledPeriod'] = description_data.attrib['polledPeriod']
-        attribute_data['description'] = attribute_properties.attrib['description']
-        attribute_data['label'] = attribute_properties.attrib['label']
-        attribute_data['unit'] = attribute_properties.attrib['unit']
+        attribute_data.update(attribute_properties.attrib)
         attribute_data['dataType'] = self._get_arg_type(description_data)
-        attribute_data['minValue'] = attribute_properties.attrib['minValue']
-        attribute_data['maxValue'] = attribute_properties.attrib['maxValue']
-        attribute_data['minAlarm'] = attribute_properties.attrib['minAlarm']
-        attribute_data['maxAlarm'] = attribute_properties.attrib['maxAlarm']
-        attribute_data['minWarning'] = attribute_properties.attrib['minWarning']
-        attribute_data['maxWarning'] = attribute_properties.attrib['maxWarning']
         return attribute_data
 
     def device_property_description_data(self, description_data):
         """Extract device property description data from the xmi tree element.
 
         Parameters
-        ---------
+        ----------
         description_data: xml.etree.ElementTree.Element
             XMI tree element with device property data
+
+            Expected element tag(s) are (i.e. description_data.tag)
+            ['DefaultPropValue']
+
+            description_data.attrib contains
+            {'description': '', 'name': 'katcp_address'}
 
         Returns
         -------
         device_property_data: dict
-            Dictionary of device property data as prescribed in the SIMDD
+            Dictionary of all device property data required
+            to create a tango device property
 
         """
-        device_property_data = {}
-        device_property_data['name'] = description_data.attrib['name']
-        device_property_data['description'] = description_data.attrib['description']
+        device_property_data = description_data.attrib
         device_property_data['type'] = self._get_arg_type(description_data)
         device_property_data['defaultPropValue'] = description_data.find(
                                                 'DefaultPropValue').text
@@ -144,7 +186,7 @@ class Xmi_Parser(object):
         """Extract argument data type from the xmi tree element.
 
         Parameters
-        ---------
+        ----------
         description_data: xml.etree.ElementTree.Element
             XMI tree element with device_property or attribute or command data
 
@@ -160,3 +202,6 @@ class Xmi_Parser(object):
             pogo_type = description_data.find('type').attrib.values()[0]
         arg_type = POGO2TANGO_TYPE[pogo_type]
         return arg_type
+
+if __name__ == '__main__':
+    Xmi_Parser('/home/athanaseus/Downloads/Weather.xmi')
