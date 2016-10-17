61d267f408478ed44fbc6a89035d60508901b59a
athanaseus <aramaila@ska.ac.za>
2016-10-17 11:45:06 +0200

    Translator code refactoring

--- mkat_tango/helper_module.py
+++ mkat_tango/helper_module.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+
 def get_server_name():
     """Gets the server name from the command line arguments
 
@@ -13,8 +14,9 @@ def get_server_name():
     ====
     Extract the server_name or equivalent executable
     (i.e.sim_xmi_parser.py -> sim_xmi_parser or
-    from the command line arguments passed, where sys.argv[1]
-    is the server instance.
+    /usr/local/bin/mkat-tango-katcpdevice2tango-DS ->
+    mkat-tango-katcpdevice2tango-DS) from the command line
+    arguments passed, where sys.argv[1] is the server instance.
 
     """
     executable_name = os.path.split(sys.argv[0].split('.')[0])[1]
