84aa4edda52cf6db9f0ffeac85ea695e33bc36a8
athanaseus <aramaila@ska.ac.za>
2016-11-17 12:05:06 +0200

    Fixing indentations the test

--- mkat_tango/simlib/tests/test_simdd_json_parser.py
+++ mkat_tango/simlib/tests/test_simdd_json_parser.py
@@ -99,31 +99,32 @@ class test_Simdd_Json_Parser(GenericSetup):
                               'rainfall', 'relative_humidity', 'wind_direction',
                               'wind_speed', 'input_comms_ok']
         actual_parsed_attr_list = actual_parsed_attrs.keys()
-        self.assertGreater(len(actual_parsed_attr_list), 0,
-            "There is no attribute information parsed")
+        self.assertGreater(
+            len(actual_parsed_attr_list), 0, "There is no attribute information parsed")
         self.assertEquals(set(expected_attr_list), set(actual_parsed_attr_list),
-            'There are missing attributes')
+                          'There are missing attributes')
 
         # Test if all the parsed attributes have the mandatory properties
         for attr_name, attribute_metadata in actual_parsed_attrs.items():
             for param in expected_mandatory_attr_parameters:
-                self.assertIn(param, attribute_metadata.keys(),
+                self.assertIn(
+                    param, attribute_metadata.keys(),
                     "The parsed attribute '%s' does not the mandotory parameter "
                     "'%s' " % (attr_name, param))
 
         # Using the made up temperature attribute expected results as we
         # haven't generated the full test data for the other attributes.
         self.assertIn('temperature', actual_parsed_attrs.keys(),
-            "The attribute pressure is not in the parsed attribute list")
+                      "The attribute pressure is not in the parsed attribute list")
         actual_parsed_pressure_attr_info = actual_parsed_attrs['temperature']
 
         # Compare the values of the attribute properties captured in the POGO
         # generated xmi file and the ones in the parsed attribute data structure.
         for prop in expected_temperature_attr_info:
             self.assertEquals(actual_parsed_pressure_attr_info[prop],
-                expected_temperature_attr_info[prop],
-                "The expected value for the parameter '%s' does not match "
-                "with the actual value" % (prop))
+                              expected_temperature_attr_info[prop],
+                              "The expected value for the parameter '%s' does "
+                              "not match with the actual value" % (prop))
 
 
 class test_PopModelQuantities(GenericSetup):
@@ -136,15 +137,14 @@ class test_PopModelQuantities(GenericSetup):
         pmq = sim_xmi_parser.PopulateModelQuantities(self.simdd_parser, device_name)
 
         self.assertEqual(device_name, pmq.sim_model.name,
-            "The device name and the model name do not match.")
+                         "The device name and the model name do not match.")
         expected_quantities_list = ['insolation', 'temperature',
                                     'pressure', 'input_comms_ok',
                                     'rainfall', 'relative_humidity',
                                     'wind_direction', 'wind_speed']
         actual_quantities_list = pmq.sim_model.sim_quantities.keys()
         self.assertEqual(set(expected_quantities_list), set(actual_quantities_list),
-            "The are quantities missing in the model")
-
+                         "The are quantities missing in the model")
 
     def test_model_quantities_metadata(self):
         """Testing that the metadata of the quantities matches with the metadata
@@ -153,7 +153,7 @@ class test_PopModelQuantities(GenericSetup):
         device_name = 'tango/device/instance'
         pmq = sim_xmi_parser.PopulateModelQuantities(self.simdd_parser, device_name)
         self.assertEqual(device_name, pmq.sim_model.name,
-            "The device name and the model name do not match.")
+                         "The device name and the model name do not match.")
         attribute_metadata = self.simdd_parser.get_reformatted_device_attr_metadata()
         for sim_quantity_name, sim_quantity in (
                 pmq.sim_model.sim_quantities.items()):
@@ -163,9 +163,9 @@ class test_PopModelQuantities(GenericSetup):
                 self.assertTrue(sim_quantity_metadata.has_key(attr_param_name),
                                 "The param '%s' was not added to the model quantity"
                                 " '%s'" % (attr_param_name, sim_quantity_name))
-                self.assertEqual(sim_quantity_metadata[attr_param_name],
-                                 attr_param_val, "The value of the param '%s' in the"
-                                 " model quantity '%s' is not the same with the one"
-                                 " captured in the SDD xml file for the monitoring"
-                                 " point '%s'." % (attr_param_name, sim_quantity_name,
-                                                   attr_param_name))
+                self.assertEqual(
+                    sim_quantity_metadata[attr_param_name], attr_param_val,
+                    "The value of the param '%s' in the model quantity '%s' is "
+                    "not the same with the one captured in the SDD xml file "
+                    "for the monitoring point '%s'." % (
+                        attr_param_name, sim_quantity_name, attr_param_name))
