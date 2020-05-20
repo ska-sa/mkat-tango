a858c27eac594cf06fbc5cdc5e62a43b694947f9
tockards <toufeeq.ockards+gitlab@gmail.com>
2020-05-20 13:33:46 +0200

    add noqa to standard_library import

--- mkat_tango/translators/tango_katcp_proxy.py
+++ mkat_tango/translators/tango_katcp_proxy.py
@@ -11,7 +11,7 @@
     @author MeerKAT CAM team <cam@ska.ac.za>
 """
 from __future__ import absolute_import, division, print_function
-from future import standard_library
+from future import standard_library # noqa: E402
 standard_library.install_aliases()  # noqa: E402
 
 import logging
